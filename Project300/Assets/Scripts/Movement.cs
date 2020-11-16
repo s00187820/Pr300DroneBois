d013fed7689f7fdbef596f2156472aef8fe25b4b
gkennedy982 <gkennedy982@gmail.com>
2020-11-16 22:43:33 +0000

    Topdown camera made
    topdown camera created

---
+++ Project300/Assets/Scripts/Movement.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    public float Speed = 20f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Input.GetAxis("Vertical") * Time.deltaTime *Speed, 0, Input.GetAxis("Horizontal")*Time.deltaTime *Speed);
+    }
+    
+}
---
+++ Project300/Assets/Scripts/TOPDOWNCamera.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TOPDOWNCamera : MonoBehaviour
+{
+    public Transform Player;
+    public float Smooth = 0.3f;
+    public float height;
+    private Vector3 velocity = Vector3.zero;
+ void Update()
+        {
+
+        Vector3 pos = new Vector3();
+        pos.x = Player.position.x;
+        pos.z = Player.position.z - 7f;
+        pos.y = Player.position.y+height;
+        transform.position = Vector3.SmoothDamp(transform.position, pos, ref velocity, Smooth);
+        }
+}
