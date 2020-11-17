a745cb5669b8d8176a197a6b169f2aa0cb77cdaa
gkennedy982 <gkennedy982@gmail.com>
2020-11-17 22:18:12 +0000

    MiniMap
    minimap created

---
+++ Project300/Assets/Scripts/MinMap.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MinMap : MonoBehaviour
+{
+    public Transform playr;
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        
+    }
+    void LateUpdate()
+    {
+        Vector3 newPos = playr.position;
+        newPos.y = transform.position.y;
+        transform.position = newPos;
+    }
+}
